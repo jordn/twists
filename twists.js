4fe81fd9f979b610ddd115a553bc7572145b737f
Jordan Burgess <jordanburgess@gmail.com>
2013-08-18 02:25:50 +0100

    Partly working membership and new logo

--- twists.js
+++ twists.js
@@ -3,6 +3,20 @@ ListList = new Meteor.Collection("listlist");
 ListMembers = new Meteor.Collection("listmembers");
 
 if (Meteor.isClient) {
+  // Google webfonts
+  WebFontConfig = {
+    google: { families: [ 'Share+Tech::latin', 'Open+Sans:400,300:latin' ] }
+  };
+  (function() {
+    var wf = document.createElement('script');
+    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+      '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
+  })();
+  //End webfonts
 
   Template.users.rendered = function () {
     console.log($(".list"));
@@ -37,39 +51,65 @@ if (Meteor.isClient) {
     });
   };
 
+
+
+  function saveOrUpdateFriendList (TwitterApiResult, callback) {
+    twitterName = Meteor.user().services.twitter.screenName
+    console.log('Updating list of friends');
+    friends = JSON.parse(TwitterApiResult.content).users;
+    friends = friends.map(function(friend){
+      console.log('mapping for each friend')
+      friendId = friend.id;
+      friend["membership"] = [];
+      if (ListList.findOne({twitterName: twitterName}).lists) {
+        console.log('There are lists')
+        lists = ListList.findOne({twitterName: twitterName}).lists;
+        for (var i = lists.length - 1; i >= 0; i--) {
+          console.log('iterating lists - list '+ lists[i].id)
+          listId = lists[i].id;
+          if (ListMembers.findOne({listId: listId})) {
+            console.log('There are list members')
+            listMembers = ListMembers.findOne({listId: listId}).listMembers;
+            for (var j = listMembers.length - 1; j >= 0; j--) {
+              console.log('iterating (list member: ' +  listMembers[j].id + ' and friendId ' + friendId);
+              if (listMembers[j].id === friendId) {
+                console.log('WE HAVE A LIST/MEMBER MATCH! listID' + lists[i].id );
+                console.log('matched list: ' + lists[i] )
+                friend["membership"].push(lists[i]);
+              };
+            };
+          } else {
+            console.log('Could not find listmembers for listId:' + listId);
+          }
+        };
+      };
+      console.log('return friend object')
+      friend["profile_image_url"] = friend["profile_image_url"].replace("_normal", "_bigger");
+      return friend;
+    });
+    FriendList.insert({userId: Meteor.userId(), twitterName: twitterName, friends: friends});
+    if (callback && typeof(callback) === "function") {  
+      callback()
+    }
+  }
+
+
   Template.users.friends = function () {
     if(Meteor.user()) {
       twitterName = Meteor.user().services.twitter.screenName
       if (!FriendList.findOne({twitterName: twitterName})) {
-        Meteor.call("FriendsList", twitterName, function(err,result) {
-          if(!err) {
-            console.log('no error friends list');
-            friends = JSON.parse(result.content).users;
-            friends = friends.map(function(friend){
-              friendId = friend.Id;
-              friend["membership"] = [];
-              lists = ListList.find({twitterName: twitterName}).lists;
-              for (var i = lists.length - 1; i >= 0; i--) {
-                listId = lists[i].id;
-                listMembers = ListMembers.findOne({listId: listId}).listMembers;
-                for (var i = listMembers.length - 1; i >= 0; i--) {
-                  if (listMembers[i].id === friendId) {
-                    friend["membership"].push(lists[i]);
-                  };
-                };
-              };
-              friend["profile_image_url"] = friend["profile_image_url"].replace("_normal", "_bigger");
-              return friend;
-            });
-            FriendList.insert({userId: Meteor.userId(), twitterName: twitterName, friends: friends});
+        Meteor.call("RequestTwitterFriends", twitterName, function(error, result) {
+          if (!error) {
+            saveOrUpdateFriendList(result);
+            return FriendList.findOne({twitterName: twitterName}).friends;
           } else {
-            // console.log(err);
-            console.log('error on freinds')
+            console.log('ERROR: ' + error);
             return false;
-          }
+          } 
         });
-      };
-      return FriendList.findOne({twitterName: twitterName}).friends;
+      } else {
+        return FriendList.findOne({twitterName: twitterName}).friends;
+      }
     } else {
       console.log('not logged in')
       return false;
@@ -82,17 +122,18 @@ if (Meteor.isClient) {
       if (!ListList.findOne({twitterName: twitterName})) {
         Meteor.call("ListsList", twitterName, function(err,result) {
           if(!err) {
-            console.log('NO error list');
+            console.log('No error grabbing your lists');
             lists = JSON.parse(result.content).lists;
             ListList.insert({userId: Meteor.userId(), twitterName: twitterName, lists: lists});
             for (var i = lists.length - 1; i >= 0; i--) {
-              console.log("LIST ID:" + lists[i].id)
-              Meteor.call("ListMembers", lists[i].id, function(err,result) {
+              listId = lists[i].id;
+              console.log("LIST ID:" + listId)
+              Meteor.call("ListMembers", listId, function(err,result) {
                 if(!err) {
                   console.log('no error list members');
                   listMembers = JSON.parse(result.content).users;
                   console.log(listMembers)
-                  ListMembers.insert({userId: Meteor.userId(), twitterName: twitterName, listId: 124123, listMembers: listMembers});
+                  ListMembers.insert({userId: Meteor.userId(), twitterName: twitterName, listId: listId, listMembers: listMembers});
                 } else {
                   console.log(err);
                   return false;
@@ -133,7 +174,7 @@ if (Meteor.isClient) {
       twitterName = Meteor.user().services.twitter.screenName;
       if (FriendList.findOne({twitterName: twitterName})) {
         friendListId = FriendList.findOne({twitterName: twitterName})._id
-        Meteor.call("FriendsList", twitterName, function(err,result) {
+        Meteor.call("RequestTwitterFriends", twitterName, function(err,result) {
           if(!err) {
             console.log('no error friends list');
             friends = JSON.parse(result.content).users;
@@ -186,8 +227,8 @@ if (Meteor.isServer) {
 
   var twitter = new Twitter();
   Meteor.methods({
-    FriendsList: function (screenName) {
-      console.log('friendslist of ' + screenName + 'requested')
+    RequestTwitterFriends: function (screenName) {
+      console.log('RequestTwitterFriends of ' + screenName + 'requested')
       if(Meteor.user()) {
         result = twitter.get('friends/list.json', {screen_name: screenName});
         return result;
